83a5e74de076f3be76ccb6e1a45a45fc52244970
Lukas Mahr <lukas@yousuckatprogramming.de>
2022-11-27 20:12:06 +0100

    Disable sql printing

--- database/database.py
+++ database/database.py
@@ -2,4 +2,4 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import registry
 
 mapper_registry = registry()
-engine = create_engine("sqlite+pysqlite:///database.db", echo=True, future=True)
+engine = create_engine("sqlite+pysqlite:///database.db", future=True)
