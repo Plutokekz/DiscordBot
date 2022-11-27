d3d2a9965bf70405e91f503ab1d888b73a357bf3
Lukas Mahr <lukas@yousuckatprogramming.de>
2022-11-27 12:57:46 +0100

    Reformat with black

--- cogs/deutschebahn/rs_api/api.py
+++ cogs/deutschebahn/rs_api/api.py
@@ -6,7 +6,9 @@ from cogs.deutschebahn.rs_api.model import CountryList, Model
 
 class RS:
     client: httpx.AsyncClient
-    base_url: str = "https://apis.deutschebahn.com/db-api-marketplace/apis/api.railway-stations.org"
+    base_url: str = (
+        "https://apis.deutschebahn.com/db-api-marketplace/apis/api.railway-stations.org"
+    )
 
     def __init__(self, client: httpx.AsyncClient):
         self.client = client
@@ -31,18 +33,27 @@ class RS:
         :param _id:
         :return:
         """
-        if (response := await self._send_get_request(f"/photoStationById/{country}/{_id}")) is not None:
+        if (
+            response := await self._send_get_request(
+                f"/photoStationById/{country}/{_id}"
+            )
+        ) is not None:
             return Model(**response)
 
-    async def get_photo_station_by_country(self, country: str, has_photo: bool = False) -> Model | Any:
+    async def get_photo_station_by_country(
+        self, country: str, has_photo: bool = False
+    ) -> Model | Any:
         """
         List stations by country
         :param has_photo:
         :param country:
         :return:
         """
-        if (response := await self._send_get_request(
-                f"/photoStationsByCountry/{country}?hasPhoto={has_photo}")) is not None:
+        if (
+            response := await self._send_get_request(
+                f"/photoStationsByCountry/{country}?hasPhoto={has_photo}"
+            )
+        ) is not None:
             return Model(**response)
 
     async def get_photo_stations_by_photographer(self, photographer: str):
@@ -51,7 +62,11 @@ class RS:
         :param photographer:
         :return:
         """
-        if (response := await self._send_get_request(f"/photoStationsByPhotographer/{photographer}")) is not None:
+        if (
+            response := await self._send_get_request(
+                f"/photoStationsByPhotographer/{photographer}"
+            )
+        ) is not None:
             return Model(**response)
 
     async def get_photo_stations_by_recent_photo_imports(self, since_hours: int = 10):
@@ -59,8 +74,11 @@ class RS:
         List stations with photos uploaded in the last 24h
         :return:
         """
-        if (response := await self._send_get_request(
-                f"/photoStationsByRecentPhotoImports?sinceHours={since_hours}")) is not None:
+        if (
+            response := await self._send_get_request(
+                f"/photoStationsByRecentPhotoImports?sinceHours={since_hours}"
+            )
+        ) is not None:
             return Model(**response)
 
     async def get_photo(self, country: str, filename: str):
@@ -70,5 +88,7 @@ class RS:
         :param filename:
         :return:
         """
-        if (response := await self._send_get_request(f"/photos/{country}/{filename}")) is not None:
+        if (
+            response := await self._send_get_request(f"/photos/{country}/{filename}")
+        ) is not None:
             return Model(**response)
